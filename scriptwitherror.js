662d708b19125541fc31b6f5c7985f72e7430770
Slava Maliy <smaliy@usatech.com>
2019-04-03 14:14:08 +0300

    script with error

---
+++ scriptwitherror.js
@@ -0,0 +1,7 @@
+(function (name, definition, context) {
+  if (typeof context['module'] != 'undefined' && context['module']['exports']) context['module']['exports'] = definition()
+  else if (typeof context['define'] != 'undefined' && context['define'] == 'function' && context['define']['amd']) define(name, definition)
+  else context[name] = definition()
+})('$script', function () {
+	let c = a.b;
+}, this);
